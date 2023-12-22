70d0a4e88b4c8c125144a4883f6eb727607bddf0
nandhakrishnanp <nandhakrishnandev@gmail.com>
2023-12-22 10:17:03 +0530

    version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,157 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { Route, Router, Routes } from 'react-router-dom';
+import './App.css';
+import Homepage from './Homepage';
+import Input from './Input';
+import Nav from './Nav';
+import axios from 'axios';
+import About from './About';
+import Contact from './Contact';
+import Resource from './Resource';
+import React, { useEffect, useState } from 'react';
+
+
+function App()  {
+   
+  const [apiResponse, setApiResponse] = useState('');
+  const [inputValue, setInputValue] = useState(''); //input
+  const [prompts,setprompt] = useState(''); // response
+  const [transcript, setTranscript] = useState(' ');
+  const [listening, setListening] = useState(false);
+
+  const [messages , setMessages] = useState([]);
+  const [isLoading,SetisLoading] = useState(false)
+ 
+  const sendInputToApi = async () => {
+    console.log(inputValue);
+    try {
+
+     
+      const userInput = inputValue ; 
+      const response = await axios.post('http://localhost:3001', { input: userInput });
+      setApiResponse(response.data);
+      console.log(apiResponse)
+      setMessages( msg => [...msg,{  msg : {inputValue}}])
+      setInputValue('');
+      SetisLoading(true)
+      
+      
+    } catch (error) {
+      console.error('Error sending input to API:', error);
+    }
+
+    const handleReq = async()=>{
+       
+      const respose = await fetch("http://localhost:3001/")
+       const result = await respose.json();
+       const prompt = result.Mainresponse.message; 
+       setprompt(prompt);
+       
+       if(prompt.length!=0){
+        SetisLoading(false)
+       setMessages( msg => [...msg,{ type: "bot", msg : {prompt}}])
+
+       }
+      
+ }
+     
+  
+    setTimeout(
+      handleReq, 2500);
+
+     
+
+  };
+
+   const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+   // console.log(inputValue);
+  };
+  const handleFormSubmit = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+  };
+  /// speech reconization
+  
+
+ 
+
+  const runSpeechRecog = () => {
+    
+     setTranscript("'Listening...")
+    const recognition = new window.webkitSpeechRecognition();
+    recognition.interimResults = true
+   
+    
+    recognition.onstart = () => {
+      setListening(true);
+    };
+     
+    recognition.continuous = true;
+    recognition.maxDuration = 15000; //15sec
+    //god its working
+    recognition.onresult = (e) => {
+      const transcript = e.results[0][0].transcript;
+      setTranscript(transcript);
+      
+    };
+
+    recognition.onend = () => {
+      setListening(false);
+      sendInputToApi(transcript)
+      console.log(transcript);
+    };
+
+    recognition.start();
+  
+
+  };
+
+  // useEffect(() => {
+  //   if (listening) {
+  //        var status =  'Listening...';
+  //   }
+  // }, [listening]);
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+         <Routes>
+       
+  <Route path='/' element={
+    <div>
+      <Nav/>
+      <Homepage
+        prompt={prompts} 
+        listening = {listening}
+        messages={messages}
+        transcript ={transcript}    //   final transcript
+        isLoading ={isLoading}
+      />
+   
+      <Input
+        runSpeechRecog ={runSpeechRecog}
+        apiResponse={apiResponse}
+        setApiResponse={setApiResponse}
+        inputValue={inputValue}
+        handleFormSubmit={handleFormSubmit}
+        handleInputChange={handleInputChange}
+        sendInputToApi={sendInputToApi}
+      />
+    </div>
+  } />
+   
+  <Route path='/About' element={<About />} />
+  <Route path='/Contact' element={<Contact />} />
+  <Route path='/Resource' element={<Resource />} />
+</Routes>
+
     </div>
   );
 }
 
-export default App;
+export default App
